63c92ff3c726c35d71c7cdb2781d596110414b87
bestfindsonline <toptierideas@gmail.com>
2025-08-07 14:15:26 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 window.TRACKER_SETTINGS = {
-  redirect_url: "https://www.amazon.com?tag=kymedia0bf-20",
+  redirect_url: "https://www.amazon.com?&linkCode=ll2&tag=kymedia0bf-20&linkId=3d0e9481de303b908b52c6f4fb9faf45&language=en_US&ref_=as_li_ss_tl",
   redirect_delay: 3,
-  webhook_url: "https://n8n.yourdomain.com/webhook/track-abc123",
+  webhook_url: "https://sturgeon-flexible-tuna.ngrok-free.app/webhook/PT-Tracker",
   heading_text: "Taking you to the product...",
-  button_text: "Hey",
+  button_text: "View on Amazon",
   disclaimer: "We may earn a commission..."
 };
