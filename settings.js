cb626b828724f507b4ec806dadae6f8ef99908f9
bestfindsonline <toptierideas@gmail.com>
2025-08-07 14:02:37 +0530

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,8 @@
+window.TRACKER_SETTINGS = {
+  redirect_url: "https://www.amazon.com?&linkCode=ll2&tag=kymedia0bf-20&linkId=6b88486d65e15afe8dd58c05904fd53d&language=en_US&ref_=as_li_ss_tl",
+  redirect_delay: 3, // in seconds
+  webhook_url: "https://n8n.yourdomain.com/webhook/track",
+  heading_text: "Taking you to the product...",
+  button_text: "Click to view",
+  disclaimer: "We may earn a commission."
+};
