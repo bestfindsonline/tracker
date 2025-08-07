4f32040ea2af64fd6f6a2cf9afdd2eecc28d8208
bestfindsonline <toptierideas@gmail.com>
2025-08-07 16:17:15 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 window.TRACKER_SETTINGS = {
   redirect_url: "https://www.amazon.com?&linkCode=ll2&tag=kymedia0bf-20&linkId=3d0e9481de303b908b52c6f4fb9faf45&language=en_US&ref_=as_li_ss_tl",
-  redirect_delay: 3,
+  redirect_delay: 1.5,
   webhook_url: "https://sturgeon-flexible-tuna.ngrok-free.app/webhook/PT-Tracker",
   heading_text: "Taking you to the product...",
   button_text: "View on Amazon",
