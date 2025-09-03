35108679204757dcd3bb8b403c4e9c2312aa0f8b
bestfindsonline <toptierideas@gmail.com>
2025-09-03 22:26:22 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 window.TRACKER_SETTINGS = {
-  redirect_url: "https://www.amazon.com?&linkCode=ll2&tag=pt-bestfindsonline-20&linkId=4fdca292edcc4c9e1d023a9a0cea5dad&language=en_US&ref_=as_li_ss_tl",
-  redirect_delay: 1.1,
+  redirect_url: "https://www.amazon.com?&linkCode=ll2&tag=kymedia00-20&linkId=3fb42c9512dc005a0b4327908351766e&language=en_US&ref_=as_li_ss_tl",
+  redirect_delay: 0.1,
   webhook_url: "https://whole-adequate-ghoul.ngrok-free.app/webhook/PT-Tracker",
   heading_text: "Taking you to the product...",
   button_text: "View on Amazon",
